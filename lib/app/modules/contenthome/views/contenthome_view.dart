ca8e71e340862eb8499621f0eb9dd1149ba59411
GigantiX <axel.ganendra157@gmail.com>
2022-09-12 20:25:00 +0700

    Profile UI 20% fix

--- lib/app/modules/contenthome/views/contenthome_view.dart
+++ lib/app/modules/contenthome/views/contenthome_view.dart
@@ -12,7 +12,30 @@ class ContenthomeView extends GetView<ContenthomeController> {
       body: Container(
         constraints: BoxConstraints.expand(),
         child: SingleChildScrollView(
-
+          child: Column(
+            children: [
+              SizedBox(
+                  height: 10
+              ),
+              Container(
+                // color: Colors.black,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1)
+                ),
+                width: double.infinity,
+                height: 55,
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    Image.asset(
+                      'assets/images/DiviAcaHrznBlck.png',
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
         ),
       )
     );
